5a9220e92108bd588d50abe8f86f53ada820d699
Maciej Kuchcik <mkuchcik@gmail.com>
2022-02-02 10:58:32 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WydawanieReszty
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Reszta r;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtKosztProduktu_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+
+
+        }
+
+        private void txtZaplacono_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            decimal koszt, zaplacono;
+
+            if (decimal.TryParse(txtKosztProduktu.Text, out koszt) && decimal.TryParse(txtZaplacono.Text, out zaplacono))
+            {
+                if (zaplacono >= koszt)
+                {
+                    decimal roznica = zaplacono - koszt;
+
+                    txtRoznicaCen.Text = roznica.ToString();
+
+                    r = new Reszta(roznica);
+
+                    txtReszta.Text = string.Join(" ", r.WydanaResztaStr());
+                }
+                else
+                {
+                    txtRoznicaCen.Text = "Koszt musi być mniejszy od kwoty zapłaconej";
+                    txtReszta.Text = "Koszt musi być mniejszy od kwoty zapłaconej";
+                }
+            }
+            else
+            {
+                txtRoznicaCen.Text = "";
+                txtReszta.Text = "";
+            }
+        }
+    }
+}
---
+++ Reszta.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace WydawanieReszty
+{
+    class Reszta
+    {
+        private decimal[] RodzajeWaluty;
+        public decimal PieniadzeDoWydania { get; set; }
+        public Reszta(decimal PieniadzeDoWydania)
+        {
+            this.PieniadzeDoWydania = PieniadzeDoWydania;
+            RodzajeWaluty = new decimal[] { 0.01M, 0.02M, 0.05M, 0.10M, 0.20M, 0.50M, 1, 2, 5, 10, 20, 50, 100, 200, 500 };
+        }
+
+        public Reszta(decimal PieniadzeDoWydania, params decimal[] _waluta)
+        {
+            this.PieniadzeDoWydania = PieniadzeDoWydania;
+            RodzajeWaluty = _waluta;
+        }
+
+        public decimal LiczaWydanReszty()
+        {
+            decimal temp = PieniadzeDoWydania;
+            decimal? liczbyWieksze = RodzajeWaluty.Where(n => n <= temp).OrderByDescending(n => n).FirstOrDefault();
+            decimal licznik = 0;
+
+            while (liczbyWieksze > 0)
+            {
+                temp -= liczbyWieksze.GetValueOrDefault(0);
+                licznik++;
+
+                liczbyWieksze = RodzajeWaluty.Where(n => n <= temp).OrderByDescending(n => n).FirstOrDefault();
+            }
+            return licznik;
+        }
+
+        public decimal[] WydanaReszta()
+        {
+            List<decimal> lista = new List<decimal>();
+            decimal temp = PieniadzeDoWydania;
+            decimal? liczbyWieksze = RodzajeWaluty.Where(n => n <= temp).OrderByDescending(n => n).FirstOrDefault();
+            decimal licznik = 0;
+
+            while (liczbyWieksze > 0)
+            {
+                temp -= liczbyWieksze.GetValueOrDefault(0);
+                licznik++;
+
+                lista.Add(liczbyWieksze.GetValueOrDefault(0));
+                liczbyWieksze = RodzajeWaluty.Where(n => n <= temp).OrderByDescending(n => n).FirstOrDefault();
+
+            }
+            return lista.ToArray();
+        }
+
+        public string[] WydanaResztaStr()
+        {
+            List<string> lista = new List<string>();
+            decimal temp = PieniadzeDoWydania;
+            decimal? liczbyWieksze = RodzajeWaluty.Where(n => n <= temp).OrderByDescending(n => n).FirstOrDefault();
+            decimal licznik = 0;
+
+            while (liczbyWieksze > 0)
+            {
+                var x = liczbyWieksze.GetValueOrDefault(0);
+
+                string wynik = "";
+
+                temp -= x;
+                licznik++;
+
+                if (x < 1)
+                {
+                    wynik = Math.Round((x * 100)).ToString() + "gr.";
+                }
+                else
+                {
+                    wynik = x.ToString() + "zł."; 
+                }
+
+
+                lista.Add(wynik);
+                liczbyWieksze = RodzajeWaluty.Where(n => n <= temp).OrderByDescending(n => n).FirstOrDefault();
+
+            }
+            return lista.ToArray();
+        }
+    }
+}
